fbefb84972b2b0fe842399e283e37bf05930ba96
zitup <yitongzhao@163.com>
2019-08-01 21:39:35 +0800

    phone commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -14,4 +14,15 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {};
+var reverseString = function(s) {
+  for (let i = 0; i < s.length / 2; i++) {
+    let temp = s[i];
+    s[i] = s[s.length - 1 - i];
+    s[s.length - 1 - i] = temp;
+    temp = null;
+  }
+};
+
+const s = ["h","e","l","l","o"];
+reverseString(s);
+console.log(s)
