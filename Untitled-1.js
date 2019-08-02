33192022bc81f8b0360fccaa455cdd9da393b798
zitup <yitongzhao@163.com>
2019-08-02 17:04:04 +0800

    new Algorithm()

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,28 +1,21 @@
 /**
- * 反转字符串
-编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
-不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
-你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
-示例 1：
-输入：["h","e","l","l","o"]
-输出：["o","l","l","e","h"]
-示例 2：
-输入：["H","a","n","n","a","h"]
-输出：["h","a","n","n","a","H"]
+ * 整数反转
+给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+示例 1:
+输入: 123
+输出: 321
+ 示例 2:
+输入: -123
+输出: -321
+示例 3:
+输入: 120
+输出: 21
+注意:
+假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/33/
  */
 /**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
+ * @param {number} x
+ * @return {number}
  */
-var reverseString = function(s) {
-  for (let i = 0; i < s.length / 2; i++) {
-    let temp = s[i];
-    s[i] = s[s.length - 1 - i];
-    s[s.length - 1 - i] = temp;
-    temp = null;
-  }
-};
-
-const s = ["h","e","l","l","o"];
-reverseString(s);
-console.log(s)
+var reverse = function(x) {};
