b95c9a14fd9e621bebb4e0576538849c5be3b739
zytjs <yitongzhao@163.com>
2019-08-06 10:35:45 +0800

    phone commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -11,11 +11,20 @@
 输入: 120
 输出: 21
 注意:
-假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+假设我们的环境只能存储得下 32 位的        有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
 https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/33/
  */
 /**
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {};
+var reverse = function(x) {
+  for(let i = 0; i < x.length / 2; i++) {
+    const temp = x.charAt(i);
+    x.charAt(i) = x.charAt(s.length - 1 - i);
+    x.charAt(x.length - 1 -i) = temp;
+  }
+  console.log(x)
+};
+
+reverse('123')
