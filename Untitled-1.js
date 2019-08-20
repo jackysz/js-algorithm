3d89bb824efe2df55c758c8f1fc1537648ab053b
zitup <yitongzhao@163.com>
2019-08-20 11:21:58 +0800

    new Algorithm("implement-strstr").KMP()

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,85 +1,27 @@
 /**
- * 实现 strStr()
-实现 strStr() 函数。
-给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ * 报数
+报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 被读作  "one 1"  ("一个一") , 即 11。
+11 被读作 "two 1s" ("两个一"）, 即 21。
+21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+
+给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
+注意：整数顺序将表示为一个字符串。
 示例 1:
-输入: haystack = "hello", needle = "ll"
-输出: 2
+输入: 1
+输出: "1"
 示例 2:
-输入: haystack = "aaaaa", needle = "bba"
-输出: -1
-说明:
-当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
-对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
-https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/38/
+输入: 4
+输出: "1211"
  */
 
 /**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
+ * @param {number} n
+ * @return {string}
  */
-var strStr = function(haystack, needle) {
-  // 方法一：利用String语法
-  // return haystack.indexOf(needle)
-  // 方法二：暴力解法
-  // if (needle === "") {
-  //   return 0;
-  // }
-  // let j = 0;
-  // for (let i = 0; i < haystack.length; i++) {
-  //   if (haystack.charAt(i) === needle.charAt(j)) {
-  //     j++;
-  //   } else {
-  //     // 如果不匹配，i置为上一轮匹配的下一位，j置为0
-  //     i = i - j;
-  //     j = 0;
-  //   }
-  //   // 全部匹配上，返回
-  //   if (j === needle.length) {
-  //     return i - j + 1;
-  //   }
-  // }
-  // return -1;
-  // 方法三：KMP解法
-  if (needle === '') {
-    return 0;
-  }
-  const next = getNext(needle);
-  let j = 0;
-  for (let i = 0; i < haystack.length; i++) {
-    if (haystack.charAt(i) === needle.charAt(j)) {
-      j++;
-    } else {
-  // 如果不匹配，i不用改变，j置为此位置最大前后缀的长度
-      j = next[j];
-    }
-    // 全部匹配上，返回
-    if (j === needle.length) {
-      return i - j + 1;
-    }
-  }
-  return -1;
-};
-
-/**
- * @param {string} str
- * @return {array}
- */
-const getNext = (str) => {
-  let j = 0;
-  let k = -1;
-  const next = [];
-  while (j < str.length) {
-    if (k === -1 || str[j] === str[k]) {
-      j++;
-      k++;
-      next[j] = k;
-    } else {
-      k = next[k];
-    }
-  }
-  return next;
-}
-
-console.log(strStr("BBC ABCDAB ABCDABCDABDE", "ABCDABD"));
+var countAndSay = function(n) {};
